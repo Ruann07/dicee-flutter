46bb09ece42b5d9f77bf7f7910364c7c5fc3c068
Ruann Lima <ruannlima07@gmail.com>
2020-04-24 17:04:44 -0300

    dicce-app completo

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.blueAccent,
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDicerNumber = 1;
+  int rightDicerNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDicerNumber = Random().nextInt(6) + 1;
+      rightDicerNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+                child: FlatButton(
+                    onPressed: () {
+                      changeDiceFace();
+                    },
+                    child: Image.asset('images/dice$leftDicerNumber.png')
+                )
+            ),
+            Expanded(
+                child: FlatButton(
+                    onPressed: () {
+                      changeDiceFace();
+                    },
+                    child: Image.asset('images/dice$rightDicerNumber.png')
+                )
+            ),
+          ],
+        )
+    );
   }
 }
